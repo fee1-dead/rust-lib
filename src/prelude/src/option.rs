966be60f29c3df60c5d5d5c0c9851628121d371e
Felix Rech <s9ferech@gmail.com>
2021-05-13 11:39:19 +0100

    Add `map_or_default` and `map_ref_or_default` on `Option` (#35)

--- src/prelude/src/option.rs
+++ src/prelude/src/option.rs
@@ -3,10 +3,14 @@
 /// Adds mapping methods to the `Option` type.
 pub trait OptionOps {
     type Item;
-    fn map_none     <F>     (self  , f:F) -> Self      where F : FnOnce();
-    fn map_ref      <U,F>   (&self , f:F) -> Option<U> where F : FnOnce(&Self::Item) -> U;
-    fn for_each     <U,F>   (self  , f:F)              where F : FnOnce(Self::Item)  -> U;
-    fn for_each_ref <U,F>   (&self , f:F)              where F : FnOnce(&Self::Item) -> U;
+    fn map_none           <F>     (self  , f:F) -> Self      where F : FnOnce();
+    fn map_ref            <U,F>   (&self , f:F) -> Option<U> where F : FnOnce(&Self::Item) -> U;
+    fn map_or_default     <U,F>   (self  , f:F) -> U
+    where U : Default, F : FnOnce(Self::Item) -> U;
+    fn map_ref_or_default <U,F>   (&self , f:F) -> U
+    where U : Default, F : FnOnce(&Self::Item) -> U;
+    fn for_each           <U,F>   (self  , f:F)              where F : FnOnce(Self::Item)  -> U;
+    fn for_each_ref       <U,F>   (&self , f:F)              where F : FnOnce(&Self::Item) -> U;
     /// Returns true if option contains Some with value matching given predicate.
     fn contains_if  <F>   (&self, f:F) -> bool      where F : FnOnce(&Self::Item) -> bool;
 }
@@ -22,6 +26,14 @@ impl<T> OptionOps for Option<T> {
 
     fn map_ref<U,F>(&self, f:F) -> Option<U> where F : FnOnce(&Self::Item) -> U {
         self.as_ref().map(f)
+    }
+
+    fn map_or_default<U,F>(self, f:F) -> U where U : Default, F : FnOnce(Self::Item) -> U {
+        self.map_or_else(U::default,f)
+    }
+
+    fn map_ref_or_default<U,F>(&self, f:F) -> U where U : Default, F : FnOnce(&Self::Item) -> U {
+        self.as_ref().map_or_default(f)
     }
 
     fn for_each<U,F>(self, f:F) where F : FnOnce(Self::Item) -> U {
